a46a5a58b391952ac65ad7524fd3e27ccf559b92
Bella9942 <gabriella.johanssson0021@stud.hkr.se>
2026-04-21 11:27:03 +0200

    Feat: initialize fullstack project structure

---
+++ README.md
@@ -0,0 +1,3 @@
+# Recipe Planner with Nutrition Data
+
+This app helps users plan recipes and view nutrition data for their meals.
---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const moongoose = require("mongoose")
+const cors = require("cors");
+const { default: mongoose } = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "API is running" });
+});
+
+mongoose
+    .connect(process.env.MONGO_URI).then (()=>{
+    console.log("Connected to Mongodb atlas");
+
+    app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+    });
+})
+    .catch((error) => {
+        console.error("MongoDB connection error:", error.message);
+    })
