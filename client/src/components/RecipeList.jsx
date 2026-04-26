4aa7dcba5bd95a892ba92de8a5a30b3692a41108
Bella9942 <gabriella.johanssson0021@stud.hkr.se>
2026-04-26 16:28:10 +0200

    feat: manage update and deletion of recipes

--- client/src/components/RecipeList.jsx
+++ client/src/components/RecipeList.jsx
@@ -5,6 +5,9 @@ function RecipeList() {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState("");
 
+    const [editingId, setEditingId] = useState(null);
+    const [editData, setEditData] = useState({});
+
     useEffect(() => {
         fetch("http://localhost:5000/api/recipes")
         .then((response) => {
@@ -29,22 +32,123 @@ function RecipeList() {
         return <p>{error}</p>;
     }
 
+    //--------------------------
+    // DELETE RECIPE
+    //--------------------------
+    const handleDelete = async (id) => {
+        const confirmDelete = window.confirm("Are you sure you want to delete this recipe?");
+
+        if (!confirmDelete) return;
+
+        try {
+            const response = await fetch(`http://localhost:5000/api/recipes/${id}`, {
+            method: "DELETE",
+            });
+
+            if (!response.ok) {
+            const data = await response.json();
+            throw new Error(data.error || "Failed to delete recipe");
+            }
+
+            setRecipes((prev) => prev.filter((recipe) => recipe._id !== id));
+        } catch (error) {
+            console.error(error.message);
+            alert(error.message);
+        }
+        };
+
+    const handleUpdate = async (id) => {
+        try {
+            const response = await fetch(`http://localhost:5000/api/recipes/${id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(editData),
+            });
+
+            const data = await response.json();
+
+            if (!response.ok) {
+            throw new Error(data.error || "Update failed");
+            }
+
+            
+            setRecipes((prev) =>
+            prev.map((recipe) => (recipe._id === id ? data : recipe))
+            );
+
+            setEditingId(null);
+
+        } catch (error) {
+            console.error(error.message);
+            alert(error.message);
+        }
+        };
+
     return (
         <div>
             <h2>Recipes</h2>
             {recipes.map((recipe) =>(
-            <div key={recipe._id}>
-                <h3>{recipe.title}</h3>
-                <p>Servings: {recipe.servings}</p>
-                <p>Calories: {recipe.totalCalories}</p> 
-                <p>Protein: {recipe.totalProtein}</p>
-                <p>Carbs: {recipe.totalCarbs}</p>
-                <p>Fat: {recipe.totalFat}</p> 
-                <p>Created by: {recipe.userId?.name}</p>
-            </div>
-        ))}
+              <div key={recipe._id}>
+
+                {editingId === recipe._id ? (
+      
+        <div>
+            <input value={editData.title}
+            onChange={(e) =>
+                setEditData({ ...editData, title: e.target.value })
+            }
+            />
+
+            <input type="number" value={editData.servings}
+            onChange={(e) =>
+                setEditData({
+                ...editData,
+                servings: Number(e.target.value),
+                })
+            }
+            />
+
+            <input value={editData.instructions}
+            onChange={(e) =>
+                setEditData({ ...editData, instructions: e.target.value })
+            }
+            />
+
+            <button onClick={() => handleUpdate(recipe._id)}>Save</button>
+            <button onClick={() => setEditingId(null)}>Cancel</button>
         </div>
-    )
-}
+
+        ) : (
+                <div key={recipe._id}>
+                    <h3>{recipe.title}</h3>
+                    <p>Servings: {recipe.servings}</p>
+                    <p>Calories: {recipe.totalCalories}</p> 
+                    <p>Protein: {recipe.totalProtein}</p>
+                    <p>Carbs: {recipe.totalCarbs}</p>
+                    <p>Fat: {recipe.totalFat}</p> 
+                    <p>Created by: {recipe.userId?.name}</p>
+                    <button onClick={() => {
+                        setEditingId(recipe._id);
+                        setEditData({
+                        ...recipe,
+                        userId: recipe.userId?._id,
+                        ingredients: recipe.ingredients.map((item) => ({
+                        ingredientId: item.ingredientId?._id,
+                        amount: item.amount}))});}}>Edit</button>
+                        
+                    <button onClick={() => handleDelete(recipe._id)}>Delete</button>
+                    
+                       </div>
+                )}
+            </div>
+            ))}
+        </div>
+        );
+        }
+                
+
+
 
 export default RecipeList;
